6a8c796580747352dd4571c57dff7e7ec834c304
Devin Jhu <devin.jhu@mths.ca>
2022-04-12 18:50:22 +0000

    code

--- any_number.py
+++ any_number.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 
 # Created by Devin Jhu
-# Created on March 2022
+# Created on April 2022
 # The area and perimeter calculator
 
 
 def main():
-    # this function calculates the area and perimeter of a rectangle
-
+    # this shows if its a square or not
     # input
-    width = int(input("Enter width of rectangle(mm): "))
-    height = int(input("Enter height of rectangle(mm): "))
+    width = int(input("Enter width: "))
+    height = int(input("Enter height: "))
 
     # process
-    area_of_rectangle = width * height
-    perimeter_of_rectangle = 2 * (width + height)
+    if width == height:
+        print("it is a square")
+    else:
+        print("it is a rectangle")
 
-    # output
-    print("")
-    print("Area is {0} mm².".format(area_of_rectangle))
-    print("Perimeter is {0} mm.".format(perimeter_of_rectangle))
-    print("")
-    print("Done.")
+    print("\nDone.")
 
 
 if __name__ == "__main__":
